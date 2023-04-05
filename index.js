e04e8923df0cf788ee5ec610b243525307f41132
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-04-04 19:42:33 -0500

    test errors solved

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const server = require("./src/config/app");
+const PORT =  process.env.PORT
 
-server.listen(process.env.PORT, () => {
+server.listen(PORT , () => {
   console.log(
-    `your application runing! check here 🔥  => http://localhost:${port}`
+    `your application runing! check here 🔥  => http://localhost:${PORT}`
   );
 });
--- src/config/variable.env.js
+++ src/config/variable.env.js
@@ -5,13 +5,11 @@ const DB_USER = process.env.DB_USERNAME;
 const DB_PASS = process.env.DB_PASSWORD;
 const DB_PORT = process.env.DB_PORT;
 const DB_URL = process.env.DB_URL;
-const APP_PORT = process.env.APP_PORT;
-const APP_ENV = process.env.APP_ENV;
+const PORT = process.env.PORT;
 const KEY_JSON = process.env.KEY_JSON;
-
-const CLOUDINARY_NAME = process.env.CD_NAME;
-const CLOUDINARY_KEY = process.env.CD_KEY;
-const CLOUDINARY_PASS = process.env.CD_PASS;
+const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME;
+const CLOUDINARY_KEY = process.env.CLOUDINARY_KEY;
+const CLOUDINARY_PASS = process.env.CLOUDINARY_PASS;
 
 module.exports = {
   DB_CONN,
@@ -21,10 +19,9 @@ module.exports = {
   DB_PASS,
   DB_PORT,
   DB_URL,
-  APP_PORT,
-  APP_ENV,
   KEY_JSON,
   CLOUDINARY_NAME,
   CLOUDINARY_KEY,
   CLOUDINARY_PASS,
+  PORT
 };

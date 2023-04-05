62c380a746d616df61eff9fa4c52e086d11959a4
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-04-05 06:53:37 -0500

    agregado librerias y verficacion de funcionalidades

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,6 @@
+const { matchedData } = require("express-validator");
 const { userModel } = require("../models");
-
+const { handlerHttpError } = require("../utils/handlerHttpError");
 /**
  * obtener la lista de usuarios
  * @param {*} req
@@ -11,10 +12,13 @@ const getAllorSearchUser = async (req, res) => {
 };
 
 const createUser = async (req, res) => {
-  const { body } = req;
+  try {
+    const body = matchedData(req);
+    const data = await new userModel(body);
 
-  const data = await userModel.create(req.body);
-  console.log(body);
-  res.send({ data });
+    res.send({ data });
+  } catch (error) {
+    handlerHttpError(res, "ERROR_FN_CREATEUSER", 500);
+  }
 };
 module.exports = { getAllorSearchUser, createUser };
--- src/middleware/handlerHttpError.js
+++ src/middleware/handlerHttpError.js
@@ -1,4 +1,10 @@
-const handlerHttpError = (res, message = "algo malo paso", code = 403) => {
+/**
+ * !TODO: Este middleware funcionara para los mensajes de errores en los controladores
+ * @param {*} res
+ * @param {*} message
+ * @param {*} code
+ */
+const handlerHttpError = (res, message = "PROCESO_NO_ESPERADO", code = 500) => {
   res.status(code);
   res.status({ error: message });
 };

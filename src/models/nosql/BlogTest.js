d7810cf6806d8a414eca4b7d26a05cec861a393b
Lautaro Heredia <103940513+LautyHeredia@users.noreply.github.com>
2023-04-07 06:08:28 -0300

    fetch: Eliminacion de handlers y controllers, implementacion del codigo que ha dicho Sergio

--- src/models/nosql/BlogTest.js
+++ src/models/nosql/BlogTest.js
@@ -1,27 +1,27 @@
-const { Schema, model, default: mongoose, Types } = require("mongoose");
+const { Schema, model, default: mongoose } = require("mongoose");
 var slug = require("mongoose-slug-generator");
 mongoose.plugin(slug);
-// const mongoose_delete = require('mongoose-delete');
+const mongooseDelete = require('mongoose-delete');
 
-const Blog = new Schema(
+const BlogSchema = new Schema(
   {
     _id: { type: Schema.Types.ObjectId, auto: true },
     title: { type: String, required: true },
     slug: { type: String, slug: "title" },
     status: {
       type: String,
-      required: true,
+      required: false,
     },
     description: { type: String, required: true },
     image: { type: String, required: true },
     author: String,
     category: [{ type: String }],
-    categories: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Category",
-      },
-    ],
+    // categories: [
+    //   {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "Category",
+    //   },
+    // ],
   },
   {
     timestamps: true,
@@ -29,9 +29,6 @@ const Blog = new Schema(
   }
 );
 
+BlogSchema.plugin(mongooseDelete, { overrideMethods: "all" })
 
-
-// Blog.plugin(mongoose_delete)
-
-
-module.exports = model("Blog", Blog);
+module.exports = model("BlogTest", BlogSchema);

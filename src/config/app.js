e856749151a55d0d2bfa7c96606fed0774186339
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-04-07 18:38:10 -0500

    config app

--- src/config/app.js
+++ src/config/app.js
@@ -7,10 +7,21 @@ const cors = require("cors");
 const routes = require("../routes");
 const server = express();
 
-server.use(cors());
+server.use(cors({ credentials: true, origin: process.env.ORIGIN }));
 server.use(express.json());
 server.use(express.static("storage"));
 server.use(morgan("dev"));
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 server.use("/", routes);
 
 module.exports = server;

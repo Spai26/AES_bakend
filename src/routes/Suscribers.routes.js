8d9827184bfa1376e1be073d8b6f511f672d84e2
Lautaro Heredia <103940513+LautyHeredia@users.noreply.github.com>
2023-04-08 05:28:18 -0300

    fetch: mejora en la ruta de suscriber

--- src/routes/Suscribers.routes.js
+++ src/routes/Suscribers.routes.js
@@ -9,8 +9,8 @@ const suscribersRoutes = Router()
 
 suscribersRoutes.get('/', getAllSuscribers)
 suscribersRoutes.post('/', postNewSuscribers)
-suscribersRoutes.delete('/delete/:id', deleteSuscribers)
+suscribersRoutes.put('/:id', updateSuscribers)
+suscribersRoutes.delete('/:id', deleteSuscribers)
 
-suscribersRoutes.put('/:id/update', updateSuscribers)
 
 module.exports = suscribersRoutes

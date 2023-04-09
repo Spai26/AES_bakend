809fd94a400c05c8cbfa09d77e999bbab3c011db
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-04-09 11:56:12 -0500

    --amend

--- src/routes/Blogs.routes.js
+++ src/routes/Blogs.routes.js
@@ -8,16 +8,21 @@ const isAuth = require("../middleware/session");
 const blogRoute = Router();
 
 blogRoute.get("/", SearchOrAllBlogs);
-blogRoute.post("/", validateNewBlog, blogController.setCreateBlog);
+blogRoute.post("/", isAuth, validateNewBlog, blogController.setCreateBlog);
 blogRoute.get("/:id", validateItem, blogController.getDetailBlog);
 blogRoute.put(
   "/:id",
+  isAuth,
   validateItem,
   validateUpdate,
   blogController.updateBlogById
 );
-blogRoute.delete("/:id", validateItem, blogController.deleteBlogLogic);
-blogRoute.post("/:id/addCategory", catWithinBlog.addCategoryToBlog);
-blogRoute.delete("/:id/deleteCategory", catWithinBlog.deleteCategoryToBlog);
+blogRoute.delete("/:id", isAuth, validateItem, blogController.deleteBlogLogic);
+blogRoute.post("/:id/addCategory", isAuth, catWithinBlog.addCategoryToBlog);
+blogRoute.delete(
+  "/:id/deleteCategory",
+  isAuth,
+  catWithinBlog.deleteCategoryToBlog
+);
 
 module.exports = blogRoute;

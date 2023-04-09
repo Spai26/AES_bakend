9ce10b859df3c1b9072c61de4b1b62284adbccd2
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-04-09 11:23:28 -0500

    quitando cors

--- src/config/app.js
+++ src/config/app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 require("./database");
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 
 const routes = require("../routes");
 const server = express();
@@ -10,16 +9,6 @@ const server = express();
 server.use(express.json());
 server.use(express.static("storage"));
 
-/* server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-}); */
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(

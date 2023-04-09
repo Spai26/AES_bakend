1783062e31ccffafb31713a326096d1cd6b58a0b
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-04-09 12:01:04 -0500

    --amend

--- src/config/app.js
+++ src/config/app.js
@@ -17,7 +17,7 @@ server.use((req, res, next) => {
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Credentials", "false");
   next();
 });
 
--- src/routes/Blogs.routes.js
+++ src/routes/Blogs.routes.js
@@ -8,20 +8,20 @@ const isAuth = require("../middleware/session");
 const blogRoute = Router();
 
 blogRoute.get("/", SearchOrAllBlogs);
-blogRoute.post("/", isAuth, validateNewBlog, blogController.setCreateBlog);
+blogRoute.post("/", validateNewBlog, blogController.setCreateBlog);
 blogRoute.get("/:id", validateItem, blogController.getDetailBlog);
 blogRoute.put(
   "/:id",
-  isAuth,
+
   validateItem,
   validateUpdate,
   blogController.updateBlogById
 );
-blogRoute.delete("/:id", isAuth, validateItem, blogController.deleteBlogLogic);
-blogRoute.post("/:id/addCategory", isAuth, catWithinBlog.addCategoryToBlog);
+blogRoute.delete("/:id", validateItem, blogController.deleteBlogLogic);
+blogRoute.post("/:id/addCategory", catWithinBlog.addCategoryToBlog);
 blogRoute.delete(
   "/:id/deleteCategory",
-  isAuth,
+
   catWithinBlog.deleteCategoryToBlog
 );
 

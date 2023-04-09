6aea9e0ea7691e4c316b2b8024794d1311044561
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-04-09 11:00:33 -0500

    credential

--- src/config/app.js
+++ src/config/app.js
@@ -10,7 +10,7 @@ const server = express();
 server.use(express.json());
 server.use(express.static("storage"));
 
-server.use((req, res, next) => {
+/* server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
@@ -18,6 +18,16 @@ server.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+}); */
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 

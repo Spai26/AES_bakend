c4e0892b92f113d49e707d9c307100b92691d8ca
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-04-09 11:02:42 -0500

    solved

--- src/config/app.js
+++ src/config/app.js
@@ -20,7 +20,7 @@ server.use(express.static("storage"));
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 }); */
-app.use((req, res, next) => {
+server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",

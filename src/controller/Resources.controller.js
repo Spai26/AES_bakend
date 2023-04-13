597473ec5e71c35014e18f56c819112dfd48febc
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-04-13 03:01:01 -0500

    controller gallery

--- src/controller/Resources.controller.js
+++ src/controller/Resources.controller.js
@@ -1,13 +1,34 @@
 const { matchedData } = require("express-validator");
 const { resources } = require("../models");
+const handlerHttpError = require("../utils/handlerHttpError");
+
 const showAllItems = (req, res) => {
-  res.send({ data: "here" });
+  try {
+    const result = resources.find({});
+
+    res.status(200).json(result);
+  } catch (error) {
+    handlerHttpError(res, "No se encontraron recursos!", 404);
+  }
 };
 
-const uploadItems = (req, res) => {
+const uploadItems = async (req, res) => {
   const { origin, url } = req.body;
-  console.log(origin, url);
-  res.send({ data: "upload" });
+
+  if (!["videos", "images"].includes(origin)) {
+    return handlerHttpError(res, "Origen invalido", 404);
+  }
+  try {
+    const data = new resources({
+      url,
+      origin,
+    });
+
+    await data.save();
+    res.send({ succes: true, data: "upload" });
+  } catch (error) {
+    handlerHttpError(res, "Archivo no subido", 500);
+  }
 };
 
 module.exports = {

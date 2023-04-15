8b5ab5b80b1c63641400cbfce103df3bc63c9b0c
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-04-15 12:57:23 -0500

    error frame

--- src/libs/validExtensionFiles.js
+++ src/libs/validExtensionFiles.js
@@ -15,13 +15,3 @@ module.exports = {
   validExtensionImage,
   validExtensionFile,
 };
-
-<iframe
-  width="560"
-  height="315"
-  src="https://www.youtube.com/embed/rumlkqm3RT8"
-  title="YouTube video player"
-  frameborder="0"
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-  allowfullscreen
-></iframe>;

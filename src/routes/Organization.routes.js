b324f02d6271c3e16ebacbf3d4cd02993c1577d7
Lautaro Heredia <103940513+LautyHeredia@users.noreply.github.com>
2023-04-14 22:06:32 -0300

    fetch: mejora y nueva ruta id y put

--- src/routes/Organization.routes.js
+++ src/routes/Organization.routes.js
@@ -1,14 +1,21 @@
 const { Router } = require('express')
 const {
-  validatorCreateOrganization  
+  validatorCreateOrganization,
+  validatorGetOrganizationById,
+  validatorPutOrganization  
 } = require('../validators/Organization')
 const {
     getAllOrganizationsForms,
-    createOrganization
+    createOrganization,
+    getOrganizationById,
+    putOrganizationById
 } = require('../controller/organization/organization.controller')
 const organizationRoutes = Router()
 
 organizationRoutes.get('/', getAllOrganizationsForms)
+organizationRoutes.get('/:id', validatorGetOrganizationById, getOrganizationById)
+
+organizationRoutes.put('/:id', validatorPutOrganization, putOrganizationById)
 organizationRoutes.post('/', validatorCreateOrganization ,createOrganization)
 
 module.exports = organizationRoutes

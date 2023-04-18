75ffb7857217714702f627de8b10846129af639f
SergioAi <53959551+Spai26@users.noreply.github.com>
2023-04-18 18:18:20 -0500

    dashboard

--- src/routes/Auth.routes.js
+++ src/routes/Auth.routes.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const AuthController = require("../controller/auth/auth.controller");
 const { validatorAuthLogin } = require("../validators/User");
 const isAuth = require("../middleware/sessionAuth");
+const handlerHttpError = require("../utils/handlerHttpError");
 
 const authRoute = Router();
 
@@ -9,7 +10,11 @@ const authRoute = Router();
 authRoute.post("/login", validatorAuthLogin, AuthController.authLogin);
 authRoute.post("/logout", AuthController.logOut);
 authRoute.get("/dashboard", isAuth, (req, res) => {
-  res.send({ message: true });
+  try {
+    res.send({ message: true });
+  } catch (error) {
+    handlerHttpError(res, "Renueva tu token", 500);
+  }
 });
 authRoute.post("/:id/reset-password", AuthController.changePassword);
 
